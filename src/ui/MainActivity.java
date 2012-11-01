172cb04dd1841c4cefc3dbb99d247dc861c4c1ca
owen <fifa0329@126.com>
2012-11-01 15:22:20 +0800

    1 ui change 2 warning if without sd card 3 upload optimization

--- src/ui/MainActivity.java
+++ src/ui/MainActivity.java
@@ -4,16 +4,22 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.lang.reflect.Method;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.ProtocolException;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.FileEntity;
 import org.apache.http.entity.InputStreamEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 import com.example.testclient.R;
 
@@ -44,7 +50,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	protected static final String UPLOAD_LOG_URL = "http://218.94.107.4:7000/WLANServer/LogCollector";
+	protected static final String UPLOAD_LOG_URL = "http://wuxiantao.sinaapp.com/wlanlog.php";
 	Button to_cmcc;
 	Button to_chinanet;
 	Button wlansetting;
@@ -52,18 +58,9 @@ public class MainActivity extends Activity {
 	Button to_else;
 	TextView ssid;
 	ConnectivityManager mConnectivityManager;
-	TextView report_total;
+	Button report_total;
 	MyApplication mApp;
-	private static final int MESSAGE_FAILED = 0;
-	private static final int MESSAGE_FINISH = 1;
-	private static final int MESSAGE_LENGTH = 2;
-	private static final int MESSAGE_PROGRESS = 3;
-	private static final int MESSAGE_UNZIP = 4;
-	private static final int MESSAGE_UPDATE = 5;
-	private static final int MESSAGE_NO_UPDATE = 6;
-	private Handler progressHandler;
 	private ProgressDialog progress;
-	private Context context;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -119,7 +116,7 @@ public class MainActivity extends Activity {
 			}
 		});
 		ssid = (TextView) findViewById(R.id.ssid);
-		report_total = (TextView) findViewById(R.id.report_total);
+		report_total = (Button) findViewById(R.id.report_total);
 
 		Button exit = (Button) findViewById(R.id.exit);
 		exit.setOnClickListener(new OnClickListener() {
@@ -148,49 +145,67 @@ public class MainActivity extends Activity {
 			}
 		});
 		
+		try {
+			if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED))
+			{
+				new File(Environment.getExternalStorageDirectory() + "/wlantest/report/").mkdirs();
+				new File(Environment.getExternalStorageDirectory() + "/wlantest/current/").mkdirs();
 
-		new File(Environment.getExternalStorageDirectory() + "/wlantest/report/").mkdirs();
-		new File(Environment.getExternalStorageDirectory() + "/wlantest/current/").mkdirs();
+				File filetotal = new File(Environment.getExternalStorageDirectory() + "/wlantest/"
+						+ "/report/");
 
-		File filetotal = new File(Environment.getExternalStorageDirectory() + "/wlantest/"
-				+ "/report/");
+				if (filetotal.listFiles().length != 0) {
+					report_total.setText("当前你已经保存" + filetotal.listFiles().length
+							+ "条报告" + "\n" + "上传请点我");
+					report_total.setOnClickListener(new OnClickListener() {
 
-		if (filetotal.listFiles().length != 0) {
-			report_total.setText("当前你已经保存" + filetotal.listFiles().length
-					+ "条报告" + "\n" + "上传请点我");
-			report_total.setOnClickListener(new OnClickListener() {
+						@Override
+						public void onClick(View arg0) {
+							// TODO Auto-generated method stub
+							final Builder builder = new AlertDialog.Builder(
+									MainActivity.this);
+							builder.setTitle("上传报告");
+							builder.setMessage("是否进行上传？");
+							builder.setPositiveButton("确定",
+									new DialogInterface.OnClickListener() {
+										public void onClick(DialogInterface dialog,
+												int which) {
+											// 进度对话框的秒显用法
+											progress = new ProgressDialog(MainActivity.this);  
+											progress.setMessage("请稍候……"); 
+											progress.show();
+											uploadLog();
+										}
 
-				@Override
-				public void onClick(View arg0) {
-					// TODO Auto-generated method stub
-					final Builder builder = new AlertDialog.Builder(
-							MainActivity.this);
-					builder.setTitle("上传报告");
-					builder.setMessage("是否进行上传？");
-					builder.setPositiveButton("确定",
-							new DialogInterface.OnClickListener() {
-								public void onClick(DialogInterface dialog,
-										int which) {
-									// 进度对话框的秒显用法
-									progress = new ProgressDialog(MainActivity.this);  
-									progress.setMessage("请稍候……"); 
-									progress.show();
-									uploadLog();
-								}
-
-							});
-					builder.setNegativeButton("取消",
-							new DialogInterface.OnClickListener() {
-								public void onClick(DialogInterface dialog,
-										int which) {
-									return;
-								}
-							});
-					builder.create().show();
-					; // 创建对话框
+									});
+							builder.setNegativeButton("取消",
+									new DialogInterface.OnClickListener() {
+										public void onClick(DialogInterface dialog,
+												int which) {
+											return;
+										}
+									});
+							builder.create().show();
+							; // 创建对话框
+						}
+					});
 				}
-			});
+			}
+			else
+			{
+				Toast.makeText(getApplicationContext(), "请挂载SD卡，否则程序无法运行",
+						Toast.LENGTH_LONG).show();
+				this.finish();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
 		}
+			
+
+		
+
+		
+
 
 	}
 
@@ -271,23 +286,27 @@ public class MainActivity extends Activity {
 	
 	// 用来监听wifi的变化，改变标题栏的报告数量
 	public void onResume() {
-		ssid.setText("无");
 		super.onResume();
+		ssid.setText("无");
 		to_cmcc.setVisibility(View.INVISIBLE);
 		to_chinanet.setVisibility(View.INVISIBLE);
 		WifiManager mWifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
 		WifiInfo mWifiInfo = mWifiManager.getConnectionInfo();
 		if(mWifiManager.isWifiEnabled())
 		{
-		ssid = (TextView) findViewById(R.id.ssid);
-		ssid.setText(mWifiInfo.getSSID());
+			if(mWifiInfo.getSSID().equals((String)""))
+			{
+				ssid.setText("无");
+			}
+			else
+			{
+			ssid.setText(mWifiInfo.getSSID());
+			}
 		}
 		// setMobileNetEnable();
-		new File(Environment.getExternalStorageDirectory() + "/wlantest/report/").mkdirs();
-		new File(Environment.getExternalStorageDirectory() + "/wlantest/current/").mkdirs();
+
 
 		// 下述代码用于根据条件判断按钮的可选择性
-		// 这里要注意，有个BUG，没有开启WIFI的话，是会出错的,因此增加了try，catch版块
 		try {
 			if (mWifiInfo.getSSID().equals((String) "CMCC")) {
 				to_cmcc.setVisibility(View.VISIBLE);
@@ -316,62 +335,129 @@ public class MainActivity extends Activity {
 		
 		
 
-		File filetotal = new File(Environment.getExternalStorageDirectory() + "/wlantest/report/");
-		if (filetotal.listFiles().length != 0) {
-			report_total.setText("当前你已经保存" + filetotal.listFiles().length
-					+ "条报告" + "\n" + "上传请点我");
+		try {
+			new File(Environment.getExternalStorageDirectory() + "/wlantest/report/").mkdirs();
+			new File(Environment.getExternalStorageDirectory() + "/wlantest/current/").mkdirs();
+			File filetotal = new File(Environment.getExternalStorageDirectory() + "/wlantest/report/");
+			if (filetotal.listFiles().length != 0) {
+				report_total.setText("当前报告数：" + filetotal.listFiles().length
+						+ "" + "\n" + "点我上传");
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
 		}
+
 
 	}
 
 	public void uploadLog() {
-		
+		/*
+		 * progress = new ProgressDialog(context); progress.setTitle("上传日志");
+		 * progress.setMessage("正在上传，请等待...");
+		 * progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		 * progress.show();
+		 */
 		new Thread(new Runnable() {
+			private int i;
+
 			@Override
 			public void run() {
+				try {
+					URL url;
+					url = new URL(UPLOAD_LOG_URL);
+					
+					
+					File file=new File(Environment.getExternalStorageDirectory()
+							+ "/wlantest/" + "/report/");
+					File[] files=file.listFiles();
+					for(int i=0;i<files.length;i++){
+						HttpURLConnection httpConn;
+						httpConn = (HttpURLConnection)url.openConnection();
+						httpConn.setDoOutput(true);
+						httpConn.setDoInput(true);
+						httpConn.setUseCaches(false);
+						httpConn.setRequestMethod("POST");
+						
+						FileInputStream inputStream = new FileInputStream(files[i].getCanonicalFile());  
+			            byte[] b = new byte[inputStream.available()];  
+			            inputStream.read(b);
 
+			    		httpConn.setRequestProperty("Content-length", Integer.toString(b.length));
+			    		httpConn.setRequestProperty("Content-Type", "application/x-zip-compressed");
+			    		httpConn.setRequestProperty("Connection", "Keep-Alive");
 
-				HttpClient client = new DefaultHttpClient();
+			    		//建立输出流，并写入数据
+			    		OutputStream outputStream = httpConn.getOutputStream();
+			    		outputStream.write(b);
+			    		outputStream.close();
+			    		//获得响应状态
+			    		int responseCode = httpConn.getResponseCode();
+			    		if(HttpURLConnection.HTTP_OK == responseCode){
+			    			Log.v("test", "" + responseCode);
+							MainActivity.this.runOnUiThread(new Runnable() {
+								@Override
+								public void run() {
+									File filetotal = new File(Environment.getExternalStorageDirectory() + "/wlantest/report/");
+									if (filetotal.listFiles().length != 0) {
+										report_total.setText("当前报告数：" + filetotal.listFiles().length
+												+ "" + "\n" + "点我上传");
+									}
+									else {
+										report_total.setText("当前报告数：0\n点我上传");
+										progress.dismiss();
+									}
+
+								}
+							});
+			    		}
+//						files[i].delete();
+//						Toast.makeText(MainActivity.this, "上传成功", Toast.LENGTH_SHORT).show();
+						try {
+							Thread.sleep(2000);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				
+
+				/*HttpClient client = new DefaultHttpClient();
 				HttpPost post = new HttpPost(UPLOAD_LOG_URL);
 				InputStreamEntity reqEntity;
 				try {
 					File file=new File(Environment.getExternalStorageDirectory()
-							+ "/wlantest/report/");
-					Log.v("report/", file.getCanonicalPath());
+							+ "/wlantest/" + "/report/");
 					File[] files=file.listFiles();
 					for(int i=0;i<files.length;i++)
 					{
 						reqEntity = new InputStreamEntity(new FileInputStream(
 								files[i].getCanonicalPath()), -1);
 						Log.v("test", "" + files[i].getCanonicalPath());
-					reqEntity.setContentType("application/x-zip-compressed");
-					reqEntity.setChunked(true);
+					//reqEntity.setContentType("application/x-zip-compressed");
+					//reqEntity.setChunked(true);
+					
+				    //post.setHeader("Content-Length", "56719");
+					post.setHeader("Accept-Encoding", "identity");
+					
 					post.setEntity(reqEntity);
 					HttpResponse response = client.execute(post);
 					Log.v("test", "" + response.getStatusLine().toString());
 					Log.v("test", "" + response.getStatusLine().getStatusCode());
 					if (response.getStatusLine().getStatusCode() == 200) {
-						MainActivity.this.runOnUiThread(new Runnable() {
-							@Override
-							public void run() {
-								Toast.makeText(MainActivity.this, "上传成功", Toast.LENGTH_SHORT).show();
-								File filetotal = new File(Environment.getExternalStorageDirectory() + "/wlantest/report/");
-								if (filetotal.listFiles().length != 0) {
-									report_total.setText("当前你已经保存" + filetotal.listFiles().length
-											+ "条报告" + "\n" + "上传请点我");
-								}
-								else {
-									report_total.setText("您目前还没有报告生成\n保存报告后会显示报告数量");
-									progress.dismiss();
-								}
-
-							}
-						});
-					}
-					files[i].delete();
-					
+//						Toast.makeText(MainActivity.this, "第"+(i+1)+"条报告已经上传成功", Toast.LENGTH_SHORT).show();
+//这条出错，不能再新线程里面动UI
 					}
 					
+					}
 
 				} catch (FileNotFoundException e) {
 					// TODO Auto-generated catch block
@@ -382,7 +468,11 @@ public class MainActivity extends Activity {
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				}
+				}*/
+				
+				
+				
+				
 
 				/*
 				 * sendMesssage(MESSAGE_FINISH, 0); } else {
@@ -393,7 +483,6 @@ public class MainActivity extends Activity {
 			}
 		}).start();
 	}
-
 
 
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
